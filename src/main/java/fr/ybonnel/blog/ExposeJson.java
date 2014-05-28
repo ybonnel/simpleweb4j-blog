d67352611acf42ee8b8ca830b861a49ec5d08054
ybonnel <ybonnel@gmail.com>
2014-05-28 14:53:44 +0200

    Add exemple of get

---
+++ src/main/java/fr/ybonnel/blog/ExposeJson.java
@@ -0,0 +1,29 @@
+package fr.ybonnel.blog;
+
+import fr.ybonnel.simpleweb4j.handlers.Response;
+
+import static fr.ybonnel.simpleweb4j.SimpleWeb4j.get;
+import static fr.ybonnel.simpleweb4j.SimpleWeb4j.start;
+
+public class ExposeJson {
+
+    private static class HelloObject {
+        String prefixe;
+        String suffixe;
+
+        private HelloObject(String prefixe, String suffixe) {
+            this.prefixe = prefixe;
+            this.suffixe = suffixe;
+        }
+    }
+
+    public static void main(String[] args) {
+        get("/hello", () -> new Response<>("Hello world"));
+
+        get("/hello/object", () -> new Response<>(new HelloObject("Hello", "World")));
+
+        get("/hello/:name", (param, routeParams) -> new Response<>("Hello " + routeParams.getParam("name")));
+
+        start();
+    }
+}
