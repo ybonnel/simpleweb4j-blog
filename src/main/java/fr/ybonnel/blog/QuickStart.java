8f58a0d781271afb396d671b7a0f9d1a2f022f57
ybonnel <ybonnel@gmail.com>
2014-05-28 09:30:20 +0200

    First commit

---
+++ src/main/java/fr/ybonnel/blog/QuickStart.java
@@ -0,0 +1,10 @@
+package fr.ybonnel.blog;
+
+import static fr.ybonnel.simpleweb4j.SimpleWeb4j.start;
+
+public class QuickStart {
+
+    public static void main(String[] args) {
+        start();
+    }
+}
